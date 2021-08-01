a95c76354f29ddf947b978e43fbab2aebc6103c1
Hanzala Arfat <hanzala735@gmail.com>
2021-08-01 12:54:44 +0530

    add home and redux user

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import Header from '../Header'
-
+import React from "react";
+import Header from "../Header";
+import Home from "../Home";
+import MenuHeader from "../MenuHeader";
 function Layout() {
-    return (
-        <div>
-            <Header />
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <MenuHeader />
+      <Home />
+    </div>
+  );
 }
 
-export default Layout
+export default Layout;
---
+++ src/components/MenuHeader/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function MenuHeader() {
+  return (
+    <div className="menuHeader">
+      <ul>
+        <li>Best Seller </li>
+        <li>Amazone Pay </li>
+        <li>Mobile </li>
+      </ul>
+    </div>
+  );
+}
+
+export default MenuHeader;
